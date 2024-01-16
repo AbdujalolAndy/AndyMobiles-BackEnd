fa8c29bba674ed2faec23cd731311cd6bad26b38
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-01-17 02:49:16 +0900

    feat: Connection, using mongoDB, to DataBase server

---
+++ server.js
@@ -0,0 +1,26 @@
+const env = require("dotenv");
+const mongoose = require("mongoose");
+const http = require("http");
+const color = require("colors/safe");
+env.config();
+
+const MONGODB_URL = process.env.MONGODB_URL || null;
+const PORT = process.env.PORT || 3001;
+mongoose.set({ strictQuery: false });
+mongoose
+  .connect(MONGODB_URL)
+  .then((data) => {
+    console.log(color.bgBlue("Connected to the server!"));
+    const app = require("./app");
+    const server = http.createServer(app);
+    server.listen(
+      PORT,
+      console.info(
+        color.bgCyan(`Backend server is listening on ${PORT} port,`),
+        color.bgMagenta(`http://localhost:${PORT}`)
+      )
+    );
+  })
+  .catch((err) =>
+    console.log(color.bgRed(`MongoDb Connection error, ${err.message}`))
+  );
