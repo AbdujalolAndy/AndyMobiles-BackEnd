afd479e0314d9a2417ce6ddfb1643fc855268cfa
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-01-22 21:31:29 +0900

    feat: Design Add Poduct Page

--- schema/productSchema.js
+++ schema/productSchema.js
@@ -74,6 +74,10 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    product_description:{
+        type:String,
+        default:""
+    }
   },
   { timestamps: true }
 );
