8f97b66b7607871aa1a4162e1ce3b7e092389352
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-04-27 07:28:14 +0900

    feat: Deploy shell creation

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-//PRODUCTION
+# PRODUCTION
 git reset --hard
 git pull origin master
 npm i
 pm2 start process.config.js --env production
 
-//DEVELOPMENT
-pm2 start process.config --env development
+# DEVELOPMENT
+# pm2 start process.config --env development
